9fa506ff270d868f38ed13266c9279ed564a1d45
Diego Alpin <e0838420@u.nus.edu>
2022-05-30 21:01:12 +0800

    Commenting lines

--- src/Main.java
+++ src/Main.java
@@ -15,17 +15,16 @@ public class Main {
     static String modifiedSentence;
     static boolean doWeContinue=true;
     public static void main(String[] args) throws IOException {
-        dictOriginal = referenceTable(); //construct original dictionary Key-Value
+        dictOriginal = referenceTable(); //1. construct original dictionary Key-Value
 
         do{
             System.out.print("Pick your offset: ");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            offset = br.readLine().toUpperCase().charAt(0);//getting offset that
+            offset = br.readLine().toUpperCase().charAt(0);//2. getting intended offset
 
+            key = getKey(dictOriginal,offset);//3. getting the key of corresponsing offset
 
-            key = getKey(dictOriginal,offset);
-
-            System.out.print("Press d for decode, e for encode: ");
+            System.out.print("Press d for decode, e for encode: ");//4. getting decode or encode
             decodeOrEncodeWord = br.readLine();
             if(decodeOrEncodeWord.length()==1){
                 decodeOrEncode = decodeOrEncodeWord.charAt(0);
@@ -46,12 +45,11 @@ public class Main {
             System.out.print("Type your sentence: ");
             mySentence = br.readLine();
 
-            if(decodeOrEncode == 'd'){
+            if(decodeOrEncode == 'd'){//5a. decode the sentence
                 modifiedSentence = decode(mySentence,offset);
                 System.out.println("Your Decoded word is "+modifiedSentence);
             }
-            else if(decodeOrEncode =='e'){
-
+            else if(decodeOrEncode =='e'){//5b. encode the sentence
                 modifiedSentence = encode(mySentence,offset);
                 System.out.println("Your encoded word is "+modifiedSentence);
             }
@@ -60,7 +58,7 @@ public class Main {
                 System.out.println("You didn't press d or e");
             }
             System.out.print("Restart? Y/N");
-            String response = br.readLine().toUpperCase();
+            String response = br.readLine().toUpperCase();//6. go back to step 2 or exit
             if(response.equals("Y")){
                 doWeContinue = true;
                 continue;
@@ -75,21 +73,21 @@ public class Main {
 
     static String encode(String plainText, Character offset){
         String[] characterArray = plainText.split("");
-        dictionary = generalTable(offset);
+        dictionary = generalTable(offset);//1. getting the offset-ed dictionary
         String word = "";
         for(String s : characterArray){
             String s1="";
             if(!s.isBlank())
             {
-                Integer x = getKey(dictOriginal,s.toUpperCase().charAt(0));
-                Character c =dictionary.get(x);
+                Integer x = getKey(dictOriginal,s.toUpperCase().charAt(0));//2. getting the keys based on original dictionary
+                Character c =dictionary.get(x);//3a. Translate the corresponding key to the value in offset-ed dict
                 s1 = String.valueOf(c);
             }
             else {
-                s1=s;
+                s1=s;// 3b. if it is space, no need translate
             }
 
-            word += s1;
+            word += s1; //4. store the word
         }
 
         return word;
@@ -98,20 +96,20 @@ public class Main {
     //meaningless to plain=decode
     static String decode(String encodedText, Character offset){
         String[] characterArray = encodedText.split("");
-        dictionary = generalTable(offset);
+        dictionary = generalTable(offset);//1. getting the offset-ed dictionary
         String word = "";
         for(String s : characterArray){
             String s1="";
 
             if(!s.isBlank()){
-                Integer x = getKey(dictionary,s.toUpperCase().charAt(0));
-                Character c =dictOriginal.get(x);
+                Integer x = getKey(dictionary,s.toUpperCase().charAt(0));//2. getting the keys based on offset-ed dictionary
+                Character c =dictOriginal.get(x);//3a. Translate the corresponding key to the value in original dict
                 s1 = String.valueOf(c);
             }
             else {
-                s1=s;
+                s1=s;// 3b. if it is space, no need translate
             }
-            word +=s1;
+            word +=s1;//4. store the word
         }
 
         return word;
@@ -132,8 +130,6 @@ public class Main {
         }
         return dictOriginal;
 
-        //System.out.println(key.get(0));
-        //System.out.println(dictOriginal);
         //  0 to 25 is A to Z     ASCII 65 to 90
         //  26 to 35 is 0 to 9    ASCII 48 to 57
         //  36 to 43 is ()*+,-./  ASCII 40 to 47
